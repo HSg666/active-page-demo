3bdc95669dcc37555825e15ca72104189ef7015a
HSg666 <1303574731@qq.com>
2022-11-28 13:35:23 +0800

    feat：完成tab栏交互效果

--- src/sessions/SecondSection/index.jsx
+++ src/sessions/SecondSection/index.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CartoonImg from '../../assets/cartoon.jpg'
 import FoodImg from '../../assets/food.jpg'
 import MovieImg from '../../assets/movie.png'
 import LifeImg from '../../assets/life.jpg'
 import styles from './styles.module.scss'
+import classNames from 'classnames'
+
+const tabs = [
+    {
+        key: 'cartoon',
+        name: '动画'
+    },
+    {
+        key: 'food',
+        name: '美食'
+    },
+    {
+        key: 'movie',
+        name: '电影'
+    },
+    {
+        key: 'lift',
+        name: '生活'
+    },
+] 
 
 const SecondSession = () => {
+    const [activeTab, setActiveTab] = useState('cartoon')
     return (
         <div className={styles.secondSection}>
             <ul>
-                <li>动画</li>
-                <li>美食</li>
-                <li>电影</li>
-                <li>生活</li>
+                {
+                    tabs.map( tab => (
+                        <li key={tab.key}  onClick={ ()=> setActiveTab(tab.key) }>
+                            <span>{ tab.name }</span>
+                            <span className={ classNames(styles.line,{ [styles.visibel]: activeTab === tab.key })  }></span>
+                        </li>
+                    ) )
+                }
+            
             </ul>
             <section className={styles.section}>
                 <h2>动画</h2>
