b318b57216a48ea87c0fa4629ec53c9e10ef0baf
HSg666 <1303574731@qq.com>
2022-11-28 11:31:50 +0800

    feat：完成tab栏的样式

--- src/sessions/SecondSection/index.jsx
+++ src/sessions/SecondSection/index.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import CartoonImg from '../../assets/cartoon.jpg'
+import FoodImg from '../../assets/food.jpg'
+import MovieImg from '../../assets/movie.png'
+import LifeImg from '../../assets/life.jpg'
+import styles from './styles.module.scss'
 
 const SecondSession = () => {
     return (
-        <div>tab栏</div>
+        <div className={styles.secondSection}>
+            <ul>
+                <li>动画</li>
+                <li>美食</li>
+                <li>电影</li>
+                <li>生活</li>
+            </ul>
+            <section className={styles.section}>
+                <h2>动画</h2>
+                <img src={CartoonImg} alt="CartoonImg" />
+            </section>
+            <section className={styles.section}>
+                <h2>美食</h2>
+                <img src={FoodImg} alt="FoodImg" />
+            </section>
+            <section className={styles.section}>
+                <h2>电影</h2>
+                <img src={MovieImg} alt="MovieImg" />
+            </section>
+            <section className={styles.section}>
+                <h2>生活</h2>
+                <img src={LifeImg} alt="LifeImg" />
+            </section>
+        </div>
     )
 }
 export default SecondSession
