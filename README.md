8a05b979e6cd719918de034552bc2bce6bda8d2d
HSg666 <1303574731@qq.com>
2022-11-29 11:08:01 +0800

    feat:完成底部广告位的显隐和编写md文档

--- README.md
+++ README.md
@@ -1,9 +1,37 @@
+### 创建项目
+
+```js
+npm create-react-app active-page-demo --template=typescript
+```
+
 ### 启动
 
 npm start
 
 ### 安装的依赖
 
--
-- npm i nodesass  css预处理器
+- npm install eslint@8.00.0 --save-exaot  避免eslint不必要的报错
+- npm i node-sass  css预处理器
 - npm i classnames   用于多个行内类存在时 其中部分样式需要动态判断  
+
+### 使用到的第三方资源
+
+- 创客贴
+- <https://cssgradient.io/>     在线生成渐变色
+
+### 技术栈
+
+React18 + sass
+
+### 项目介绍
+
+- 模仿哔哩哔哩移动端活动页
+  - 功能如下
+     1、滑动时自动高亮对应的tab栏
+     2、点击tab跳转到对应的section
+     3、根据tab滑动的距离自动吸顶
+     4、tab栏根据每个section滑动的距离显隐广告位(带过渡动画的)
+
+### 教程网址
+
+<https://www.bilibili.com/video/BV1yW4y1H7JZ/?spm_id_from=333.1007.top_right_bar_window_custom_collection.content.click&vd_source=ac6f954f925d420ce11cce0f0cdd6566>
--- src/sessions/SecondSection/index.jsx
+++ src/sessions/SecondSection/index.jsx
@@ -3,8 +3,10 @@ import CartoonImg from '../../assets/cartoon.jpg'
 import FoodImg from '../../assets/food.jpg'
 import MovieImg from '../../assets/movie.png'
 import LifeImg from '../../assets/life.jpg'
+import LogoImg from '../../assets/logo.png'
 import styles from './styles.module.scss'
 import classNames from 'classnames'
+
 
 const tabs = [
     {
@@ -38,10 +40,10 @@ const SecondSession = () => {
     /*
     自动吸顶
         1、给页面盒子的添加由useRef生成的动态ref值  目的拿到标签元素
-        2、用滚动函数监听它的滚动距离，如果有就获取它的滚动top值，滚动top值<=0时说明到顶了，此时将它的判断值isFixed设置为true
+        2、用滚动函数监听它的滚动距离，如果有就获取它的滚动top值，滚动top值<=30时说明到顶了，此时将它的判断值isFixed设置为true
         3、在全局useEffect中监听同时销毁
         4、给tab添加classnames动态判断样式  如果isFixed为true说明到顶了，则给它添加isFixed样式  isFixed样式为固定定位 
-            同时给tab的默认样式添加绝对定位，同时最外层盒子的padding-top设置为tab的高度
+            同时给tab的默认样式添加绝对定位，以及最外层盒子的padding-top设置为tab的高度
         5、由于tab吸顶后内容的标题被ta盖住了，此时可以给内容盒子加一个滚动外边距  section scroll-margin:56px  isFixed类添加背景浅黄色
    */
   const onScroll = () => {
@@ -68,7 +70,7 @@ const SecondSession = () => {
   }
     /* 点击tab跳转到对应的内容
         1、给每个tab下的内容盒子添加单独的id
-        2、点击每个tab栏将其名称传到activeTed函数中，先将被选中的变量值更改为当前点中的
+        2、点击每个tab栏将其名称传到activeTed函数中，先将被选中的变量值赋值给当前点中时存储的变量的值
         3、再动态获取被点击的section标签
         4、将被点击的标签滚动到顶部
     */
@@ -94,6 +96,10 @@ const SecondSession = () => {
 
   /*
     底部固定app打开  当isFixed值为true时则显示  否则隐藏  显示时动态添加visible样式   同样用classNames动态判断 
+     
+     过渡效果:  一种从下而上的显隐动画
+      默认整个盒子为向下偏移64px,当isFixed为true时添加visibel样式(向下偏移量则为0)  
+      同时默认盒子隐藏时有过渡效果,transition为透明度 0.3s后展示,类型为偏移transform 也是0.3s
   */
 
 
@@ -116,10 +122,14 @@ const SecondSession = () => {
                         <h2>{ tab.name }</h2>
                         <img src={tab.img} alt={tab.name} />
                     </section>
-                    
                 ) )
             }
-          
+            <div className={classNames(styles.appTips, { [styles.appTips_visible]: isFixed }) }>
+                <div className={styles.pic}>
+                    <img src={LogoImg} alt="" />
+                </div>
+                <button><a href="http://www.baidu.com">App中打开</a></button>
+            </div>
         </div>
     )
 }
